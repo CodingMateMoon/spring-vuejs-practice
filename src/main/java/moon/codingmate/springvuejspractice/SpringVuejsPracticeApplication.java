2f654ff304a9105f1bfc52dd40520eb7a2d16c04
codingmatemoon <abc9529@naver.com>
2022-11-12 10:22:37 +0900

    Initial commit

---
+++ src/main/java/moon/codingmate/springvuejspractice/SpringVuejsPracticeApplication.java
@@ -0,0 +1,13 @@
+package moon.codingmate.springvuejspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVuejsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVuejsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moon/codingmate/springvuejspractice/SpringVuejsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package moon.codingmate.springvuejspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVuejsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
